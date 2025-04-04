dcd8661478884e6b6a8f9064e15182a72b89553f
tails <169904231+kanata-05@users.noreply.github.com>
2025-04-04 20:22:42 +0900

    made it more readable and abstract

--- Factorial.py
+++ Factorial.py
@@ -1,16 +1,24 @@
 import sys 
 sys.set_int_max_str_digits(2147483647)
-fac = int(input())
 
-facnum = list()
-facv = 1
-for i in range(1, fac+1):
-    facnum.append(fac)
-    fac-=1
+def getPrecedingDigits(num):
+    precedingDigits = list()
+    for i in range(1, num+1):
+        precedingDigits.append(num)
+        num-=1
+    return precedingDigits
 
-for i in range(1, len(facnum)+1):
-    facv *= facnum[i-1]
-    i-=1
+def multiplyListObjects(list):
+    factorialValue=1
+    for i in range(1, len(list)+1):
+        factorialValue *= list[i-1]
+        i-=1
+    return factorialValue
 
-print(facv) 
+def factorial(num):
+    precedingDigits = getPrecedingDigits(num)
+    factorialValue = multiplyListObjects(precedingDigits)
+    return factorialValue
+
+print(factorial(int(input()))) 
  
